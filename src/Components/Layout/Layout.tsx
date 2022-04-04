9f5e36d4710fad4736423d5a65ae6733019525fd
Andrew Chander <13403032+taghead@users.noreply.github.com>
2022-04-04 21:02:03 +1000

    Site under construction messege

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -5,6 +5,7 @@ export const Layout = ({ children }: any) => {
     <div className="layout">
       <div className="flex space-between">
         <div>Andrew Chander</div>
+        <div>Site is currently under construction.</div>
         <div>Github | Twitter | LinkedIn | WakaTime </div>
       </div>
       <main>{children}</main>
